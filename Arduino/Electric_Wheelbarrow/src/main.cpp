9cf69ed3240388b00c6b359abbcaa12aca02b10f
MxFxM <mxfxmmueller@gmail.com>
2020-06-30 18:11:51 +0200

    Hal Sensor instead of Rotary Encoder

--- Arduino/Electric_Wheelbarrow/src/main.cpp
+++ Arduino/Electric_Wheelbarrow/src/main.cpp
@@ -1,94 +1,33 @@
 #include <Arduino.h>
 
-// these are the encoder inputs
-#define ENC_A 5
-#define ENC_B 4
+#define HAL A1
 
-#define MAX_COUNTER_VALUE 2500
-#define MIN_COUNTER_VALUE 0
+#define MAX_COUNTER_VALUE 3000
+#define MIN_COUNTER_VALUE 300
 
-void read_encoder(void);
-
-volatile int counter = 500;
-volatile int previousCounter = 0; // forces update on startup
+int min_trottle = 0;
 
 void setup()
 {
-  // pwm output pin
-  pinMode(3, OUTPUT);
+  pinMode(HAL, INPUT);
 
-  pinMode(7, INPUT_PULLUP);
-
-  // configure the pins as input
-  pinMode(ENC_A, INPUT);
-  pinMode(ENC_B, INPUT);
-
-  // all changes on both inputs trigger the interrupt
-  //attachInterrupt(ENC_A, read_encoder, CHANGE);
-  //attachInterrupt(ENC_B, read_encoder, CHANGE);
+  delay(500);
+  min_trottle = analogRead(HAL) + 50;
 
   Serial.begin(115200);
 }
 
 void loop()
 {
-  read_encoder();
+  int val = analogRead(HAL);
 
-  // only update when the counter has changed
-  if (counter != previousCounter) {
-    previousCounter = counter;
+  if (val < min_trottle) {
+    val = 0;
+  } else {
+    val = int(map(val, min_trottle, 1023, MIN_COUNTER_VALUE, MAX_COUNTER_VALUE));
   }
 
-  if (digitalRead(7) == LOW) {
-    Serial.print("v 0 ");
-    Serial.print(counter);
-    Serial.print(" 0\n");
-    delay(100);
-  }
-}
-
-// interrupt for reading the rotary encoder
-void read_encoder()
-{
-  static uint8_t previousState = 3;
-  static boolean tick = false;
-
-  // the teensy has fast digital inputs
-  uint8_t newA = digitalRead(ENC_A);
-  uint8_t newB = digitalRead(ENC_B);
-
-  // add both inputs to get an integer for the phase
-  uint8_t newState = (newA << 1) | newB;
-
-  // only if the state has changed (might not be true for bouncing signals)
-  if (newState != previousState) {
-    // return to neutral state from one direction
-    if (newState == 3 && previousState == 2 && tick) {
-      counter = counter + 20;
-      // limit
-      if (counter >= MAX_COUNTER_VALUE) {
-        counter = MAX_COUNTER_VALUE;
-      }
-      tick = false;
-
-    // return to neutral from the other direction
-    } else if (newState == 3 && previousState == 1 && tick) {
-      counter = counter - 20;
-      // limit
-      if (counter <= MIN_COUNTER_VALUE) {
-        counter = MIN_COUNTER_VALUE;
-      }
-      tick = false;
-
-    // only count when the midpoint of a click has passed
-    } else if (newState == 2 && previousState == 0 && !tick) {
-      tick = true;
-    } else if (newState == 1 && previousState == 0 && !tick) {
-      tick = true;
-    } else {
-      tick = false;
-    }
-
-    previousState = newState;
-  }
+  Serial.print("v 0 ");
+  Serial.print(val);
+  Serial.print(" 0\n");
 }
